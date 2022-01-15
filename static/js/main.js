512b3a1b0b9e0d934bcb174a815f4fad19a88938
moyosore1 <35351716+moyosore1@users.noreply.github.com>
2022-01-15 16:18:42 -0500

    added agora

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+const APP_ID = "2a962873811a43968b470dc3a38fa074";
+const channel = "main";
+const token =
+  "0062a962873811a43968b470dc3a38fa074IAAebRnV6Me9BIjRyTg3VC8Ttyx31ZXl/F2ULCj2YknWO2TNKL8AAAAAEACBgxuC3IfkYQEAAQDch+Rh";
+
+
+  // create client object from agora
+
+  const client = AgoraRTC.createClient({mode: })
